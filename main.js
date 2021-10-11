31437671d14a3d969eb63334a4e7452a55ff9288
PaoloPascucci <pascuccip9@gmail.com>
2021-10-11 14:49:00 +0200

    Add decimali

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 const UserAge = prompt("Quanti anni hai?")
 const UserKm = prompt("Quanti chilometri devi fare?")
 
-console.log(UserAge + UserKm)
 
-let ticket_price = "Il prezzo é:" + (0.21 * UserKm) + "€"
-
-console.log(ticket_price)
+let ticket_price = "Il prezzo é: " + (0.21 * UserKm).toFixed(2) + " €";
 
 if (UserAge <= "17") {
 
-    var final_price = "Il prezzo scontato é:" + ((0.21 * UserKm) - (((0.21 * UserKm) * 20) / 100)) + "€"
+    var final_price = "Il prezzo scontato é: " + ((0.21 * UserKm) - (((0.21 * UserKm) * 20) / 100)).toFixed(2) + " €"
 
-    console.log(final_price)
+    console.log(final_price);
+
 }
-else if (UserAge >= "65")
+else if (UserAge >= "65") {
 
-    var final_price = "Il prezzo scontato é:" + ((0.21 * UserKm) - (((0.21 * UserKm) * 40) / 100)) + "€"
+    var final_price = "Il prezzo scontato é: " + ((0.21 * UserKm) - (((0.21 * UserKm) * 40) / 100)).toFixed(2) + " €"
 
-console.log(final_price)
+    console.log(final_price);
+}
+else {
+    var final_price = ticket_price
+
+    console.log(final_price);
+}
+
