d23c3f5c10e15b2bbe8b567a9a6fa94e7029501a
PaoloPascucci <pascuccip9@gmail.com>
2021-10-11 14:00:56 +0200

    Add Prezzo biglietto e sconti

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ const UserAge = prompt("Quanti anni hai?")
 const UserKm = prompt("Quanti chilometri devi fare?")
 
 console.log(UserAge + UserKm)
+
+let ticket_price = "Il prezzo é:" + (0.21 * UserKm) + "€"
+
+console.log(ticket_price)
+
+if (UserAge <= "17") {
+
+    var final_price = "Il prezzo scontato é:" + ((0.21 * UserKm) - (((0.21 * UserKm) * 20) / 100)) + "€"
+
+    console.log(final_price)
+}
+else if (UserAge >= "65")
+
+    var final_price = "Il prezzo scontato é:" + ((0.21 * UserKm) - (((0.21 * UserKm) * 40) / 100)) + "€"
+
+console.log(final_price)
